4cc7005d6917bceb9b31afb91620976b394ebef5
pbergsmann <p.bergsmann@opendo.at>
2014-11-15 13:40:38 +0100

    [BUGFIX] Fixes wrong TYPO3 version-dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
